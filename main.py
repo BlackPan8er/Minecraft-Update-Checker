4523327129458eade470a2a71fd08ea956ca6aa6
BlackPan8er <harismastergamer@gmail.com>
2025-11-02 18:27:50 +0200

    Added comments until line 42

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from loguru import logger
-import sys
-import readchar
-import os
+from loguru import logger #logger (duh)
+import sys                # needed by the logger to be used as a sink (output)
+import readchar           # needed for the menus, detects when a key is pressed, like up-arrow
+import os                 # needed for the menus to clear the screen, also will be needed by other stuff
 
 DEBUG = True
 
@@ -10,36 +10,36 @@ logger.add(sys.stderr, format="|{time}| - |{level}| - | {message}", level="DEBUG
 
 
 
-class MenuClass:
+class MenuClass: #Inside these are all the menus, this make it easy to trigger menus by doing menu.menuyouwanttotrigger.execute()
 
-    class MainClass:
+    class MainClass:      #main menu, then one with the choices 'Edit Projects' and 'quit'
         def __init__(self):
-            self.Options = ["Edit Projects", "Quit"]
-            self.CurOpt = 0
+            self.Options = ["Edit Projects", "Quit"] #the options of the menu
+            self.CurOpt = 0 #the current option selected
         
-        def Execute(self):
+        def Execute(self): #called to trigger (execute) the menu
             while True:
                 menu.clear() #clear screen
-                for i in range(len(self.Options)):
+                for i in range(len(self.Options)): #goes and prints every option on a new line, also adds a prefix (>>> ) to the selected option (CurOpt)
                     prefix = ">>>> " if self.CurOpt == i else "     "
                     title = self.Options[i]
                     print(f"{prefix}{title}")
 
-                if self.HandleInput() == True: #returned only if enter was pressed
-                    if self.CurOpt == 0:
+                if self.HandleInput() == True: #Handles input (duh), see below for more. Returns True only if Enter is pressed, then we do the corresponding action to the current option selected, if its false we continue the While True loop
+                    if self.CurOpt == 0: # Corresponds to Edit Projects
                         menu.EditProjectsMenu.execute()    
-                    elif self.CurOpt == 1:
+                    elif self.CurOpt == 1: # Corresponds to Quit
                         quit(0)
                 
 
         
-        def HandleInput(self):
-            key = readchar.readkey()
-            if key == readchar.key.UP and self.CurOpt != 0:
-                self.CurOpt -= 1
-            elif key == readchar.key.DOWN and self.CurOpt != len(self.Options):
+        def HandleInput(self): #Reads which key is pressed, then adjusts the CurOpt as needed
+            key = readchar.readkey() #reads the key
+            if key == readchar.key.UP and self.CurOpt != 0: # if up is pressed and its not already on the top-most option then go one option up
+                self.CurOpt -= 1 
+            elif key == readchar.key.DOWN and self.CurOpt != len(self.Options): #if down is pressed and its not already on the down-most option then go one option down
                 self.CurOpt +=1
-            elif key == readchar.key.ENTER:
+            elif key == readchar.key.ENTER: #if enter is pressed return True, then the above function runs the corresponding action
                 return True
     
     class EditProjectsMenuClass:
